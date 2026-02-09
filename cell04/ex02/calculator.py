1459a5b19e5a4e9b5f765f40a62fc6f10abbd78f
66050452-jpg <66050452@kmitl.ac.th>
2026-02-09 19:22:13 +0700

    Delete cell04/ex02 directory

--- cell04/ex02/calculator.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Give me the first number: "))
-b = int(input("Give me the second number: "))
-
-print("Thank you!")
-print(f"{a} + {b} = {a + b}")
-print(f"{a} - {b} = {a - b}")
-print(f"{a} / {b} = {a // b}")
-print(f"{a} * {b} = {a * b}")
